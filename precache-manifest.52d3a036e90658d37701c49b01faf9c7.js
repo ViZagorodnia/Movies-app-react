c1386941473daded84e35baca4add676b2a337b4
ViZagorodnia <viktoriazagorodnia@gmail.com>
2020-07-06 10:52:03 +0300

    Updates

---
+++ precache-manifest.52d3a036e90658d37701c49b01faf9c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b202804e483623dbc514a589ae4b060c",
+    "url": "/Movies-app-react/index.html"
+  },
+  {
+    "revision": "bb374e7280da58820056",
+    "url": "/Movies-app-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "90a3e0cd3cd47370cea9",
+    "url": "/Movies-app-react/static/css/main.8612f3c3.chunk.css"
+  },
+  {
+    "revision": "bb374e7280da58820056",
+    "url": "/Movies-app-react/static/js/2.838d2467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-app-react/static/js/2.838d2467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a3e0cd3cd47370cea9",
+    "url": "/Movies-app-react/static/js/main.13af82af.chunk.js"
+  },
+  {
+    "revision": "db3d29ddd798349ade39",
+    "url": "/Movies-app-react/static/js/runtime-main.a03c8bb1.js"
+  }
+]);
